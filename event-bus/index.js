25000ed73afba516a81baa275787f18a5fa26b9e
unknown <amlkamel132@gmail.com>
2022-08-20 22:12:02 +0200

    moderation

--- event-bus/index.js
+++ event-bus/index.js
@@ -5,19 +5,36 @@ const axios = require("axios");
 const app = express();
 app.use(bodyParser.json());
 
+const events = [];
+
 app.post("/events", (req, res) => {
   const event = req.body;
 
-  axios.post("http://localhost:4000/events", event).catch((err) => {
+  events.push(event);
+
+  axios.post("http://localhost:4000/events", event).catch((err) => { // post-service
+    
     console.log(err.message);
   });
-  axios.post("http://localhost:4001/events", event).catch((err) => {
+  axios.post("http://localhost:4001/events", event).catch((err) => {  // comment-service
+   
     console.log(err.message);
   });
-  axios.post("http://localhost:4002/events", event).catch((err) => {
+  axios.post("http://localhost:4002/events", event).catch((err) => {  // query-service
+   
     console.log(err.message);
   });
+
+  axios.post("http://localhost:4003/events", event).catch((err) => { // moderation-service
+    
+    console.log(err.message);
+  });
+
   res.send({ status: "OK" });
+});
+
+app.get("/events", (_req, res) => {
+  res.send(events);
 });
 
 app.listen(4005, () => {
